50397c832ede27e2ee7b9a2c0a3dea01865a91ba
Felipe A. Costa <felipeagc2@gmail.com>
2024-12-23 22:33:01 -0300

    Improve transaction filters for pix transfers

--- packages/core/src/tx_filters.ts
+++ packages/core/src/tx_filters.ts
@@ -2,31 +2,65 @@ import { Transaction } from "pluggy-sdk";
 
 export type TxFilter = (tx: Transaction) => Transaction | undefined;
 
-export function getFilterForBank(fid: number): TxFilter | undefined {
+export function getFilterForBank(fid: number): TxFilter {
   switch (fid) {
     case 1:
-      return bbFilter;
+      return bbFilter(pixFilter(identityFilter));
+    case 260:
+      return pixFilter(identityFilter);
     case 208:
-      return btgFilter;
+      return pixFilter(identityFilter);
     default:
+      return identityFilter;
+  }
+}
+
+function bbFilter(next: TxFilter): TxFilter {
+  return (tx) => {
+    if (tx.description.match(/^(PGTO DEBITO CONTA|REND\.FACIL|RENDE FACIL)/g)) {
       return undefined;
-  }
-}
-
-function bbFilter(tx: Transaction): Transaction | undefined {
-  if (tx.description.match(/^(PGTO DEBITO CONTA|REND\.FACIL|RENDE FACIL)/g)) {
-    return undefined;
-  }
-  return tx;
-}
-
-function btgFilter(tx: Transaction): Transaction | undefined {
-  if (tx.paymentData?.paymentMethod === "PIX") {
-    if (tx.type === "CREDIT" && tx.paymentData.payer?.name) {
-      tx.description = `Pix de ${tx.paymentData.payer.name}`;
-    } else if (tx.type === "DEBIT" && tx.paymentData.receiver?.name) {
-      tx.description = `Pix para ${tx.paymentData.receiver.name}`;
     }
+    return next(tx);
   }
-  return tx;
 }
+
+function pixFilter(next: TxFilter): TxFilter {
+  return (tx) => {
+    const newTx = { ...tx };
+
+    if (tx.paymentData) {
+      const isTransfer = tx.paymentData.paymentMethod === "PIX" || tx.paymentData.paymentMethod === "TEF";
+      const transferType = tx.paymentData.paymentMethod;
+
+      if (isTransfer) {
+        if (tx.type === "CREDIT" && tx.paymentData.payer) {
+          const hasAccountInfo = tx.paymentData.payer.routingNumber && tx.paymentData.payer.accountNumber && tx.paymentData.payer.branchNumber;
+
+          if ((tx.paymentData.payer.name || tx.paymentData.payer.documentNumber?.value) && hasAccountInfo) {
+            newTx.description = `${transferType} de ${tx.paymentData.payer.name ?? tx.paymentData.payer.documentNumber?.value}`;
+            if (hasAccountInfo) {
+              newTx.description += ` (${tx.paymentData.payer.routingNumber}/${tx.paymentData.payer.branchNumber}/${tx.paymentData.payer.accountNumber})`;
+            }
+          }
+        } else if (tx.type === "DEBIT" && tx.paymentData.receiver) {
+          const hasAccountInfo = tx.paymentData.receiver.routingNumber && tx.paymentData.receiver.accountNumber && tx.paymentData.receiver.branchNumber;
+
+          if ((tx.paymentData.receiver.name || tx.paymentData.receiver.documentNumber?.value) && hasAccountInfo) {
+            newTx.description = `${transferType} para ${tx.paymentData.receiver.name ?? tx.paymentData.receiver.documentNumber?.value}`;
+            if (hasAccountInfo) {
+              newTx.description += ` (${tx.paymentData.receiver.routingNumber}/${tx.paymentData.receiver.branchNumber}/${tx.paymentData.receiver.accountNumber})`;
+            }
+          }
+        }
+      } else {
+        if (tx.paymentData?.receiver?.routingNumber) {
+          console.log(tx);
+        }
+      }
+    }
+
+    return next(newTx);
+  };
+}
+
+function identityFilter(tx: Transaction): Transaction { return tx; }
